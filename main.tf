21aaee0ed48a094b3738d24c315fc37be21bd5fb
sunil kanaujiya <52691509+sunil9889@users.noreply.github.com>
2025-09-21 17:21:42 +0530

    Update main.tf
    ec2 instance in ap-south-1

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1"  
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0144277607031eca2" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Terraform-EC2"
   }
 }
